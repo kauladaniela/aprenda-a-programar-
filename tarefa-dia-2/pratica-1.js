7b30b9938bbc2ee18f3bcfda744afc1759068e84
Kaula <kauladaniela@gmail.com>
2022-08-24 21:50:59 -0300

    arquivo alterado

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,12 @@
+let nome = "Kaula"
+let email = "kauladaniela@gmail.com"
+let profissao = "Designer"
+let idade = 28
+let sonho = "Roma"
+
+var string nome,email, profissao,sonho
+var number idade
+
+let mensagem = "Ola, "+nome+!" Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para " + sonho +". Confirma pra gente alguns dados? A sua idade é " + idade +"  , você é " + profissao +" e o seu email é " + email+ "? Ficamos no aguardo. Muito obrigado e boa viagem! :D  "
+
+console.log()
