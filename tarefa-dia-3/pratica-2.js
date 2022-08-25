9e6e2ae8e0154402747fe5102c82d347d44b3e3b
Kaula <kauladaniela@gmail.com>
2022-08-25 15:43:34 -0300

    arquivo modificado

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,10 @@
+let multiplicador = 9
+let i = 1
+let multiplicando = 9
+
+while (i <= multiplicando) {
+    let tab = multiplicador * i
+    let textoex = `${multiplicador} X ${i} = ${tab}`
+    console.log(textoex)
+    i++
+}
